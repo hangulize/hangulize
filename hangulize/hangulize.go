df55e68f0e15aa8f7ce88bc4e65e1d8dc32eec94
Heungsub Lee <sub@subl.ee>
2018-05-21 00:22:47 +0900

    Init hangulize package

---
+++ hangulize/hangulize.go
@@ -0,0 +1,25 @@
+/*
+Package hangulize provides an automatic transcriber into Hangul for non-Korean
+words.
+
+Originally, Hangulize was implemented in Python in 2010.  This implementation
+is a reboot with attractive feature improvements.
+
+Original implementation: https://github.com/sublee/hangulize
+
+Brian Jongseong Park proposed the seed idea of Hangulize on his Blog.
+
+Post by Brian: http://iceager.egloos.com/2610028
+
+*/
+package hangulize
+
+// Hangulize transcribes a non-Korean word into Hangul, the Korean alphabet:
+//
+//    Hangulize("ita", "gloria")
+//    // Output: "글로리아"
+//
+func Hangulize(lang string, word string) string {
+	// TODO(sublee): implement
+	return ""
+}
