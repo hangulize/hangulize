7c864a148bc4e532fb68c0a19ec57d65e51b18e3
Heungsub Lee <sub@subl.ee>
2018-05-21 02:14:38 +0900

    Refactor Spec

--- hangulize/spec.go
+++ hangulize/spec.go
@@ -21,6 +21,10 @@ type Spec struct {
 	test []hgl.Pair
 }
 
+func (s *Spec) String() string {
+	return fmt.Sprintf("<Spec lang=%s>", s.lang.id)
+}
+
 // Language identifies a natural language.
 type Language struct {
 	id      string
@@ -45,22 +49,54 @@ func ParseSpec(r io.Reader) (*Spec, error) {
 		return nil, errors.Wrap(err, "failed to parse HGL source")
 	}
 
-	return buildSpec(h)
+	// lang
+	lang, err := buildLanguage(h)
+	if err != nil {
+		return nil, err
+	}
+
+	// config
+	config, err := buildConfig(h)
+	if err != nil {
+		return nil, err
+	}
+
+	// vars (optional)
+	vars := buildVars(h)
+
+	// rewrite
+	rewrite, err := buildRewrite(h)
+	if err != nil {
+		return nil, err
+	}
+
+	// hangulize
+	hangulize, err := buildHangulize(h)
+	if err != nil {
+		return nil, err
+	}
+
+	// test (optional)
+	test := buildTest(h)
+
+	spec := Spec{
+		*lang,
+		*config,
+		vars,
+		rewrite,
+		hangulize,
+		test,
+	}
+	return &spec, nil
 }
 
-func buildSpec(h hgl.HGL) (*Spec, error) {
-	var sec hgl.Section
-	var list *hgl.ListSection
-	var dict *hgl.DictSection
-	var ok bool
-
-	// lang
-
-	sec, ok = h["lang"]
+func buildLanguage(h hgl.HGL) (*Language, error) {
+	sec, ok := h["lang"]
 	if !ok {
 		return nil, errors.New("'lang' section required")
 	}
-	dict = sec.(*hgl.DictSection)
+	dict := sec.(*hgl.DictSection)
+
 	lang := Language{
 		id:      dict.One("id"),
 		code:    dict.All("code"),
@@ -68,14 +104,15 @@ func buildSpec(h hgl.HGL) (*Spec, error) {
 		korean:  dict.One("korean"),
 		script:  dict.One("script"),
 	}
+	return &lang, nil
+}
 
-	// config
-
-	sec, ok = h["config"]
+func buildConfig(h hgl.HGL) (*Config, error) {
+	sec, ok := h["config"]
 	if !ok {
 		return nil, errors.New("'config' section required")
 	}
-	dict = sec.(*hgl.DictSection)
+	dict := sec.(*hgl.DictSection)
 
 	// a marker must be 1-character.
 	stringMarkers := dict.All("markers")
@@ -94,50 +131,45 @@ func buildSpec(h hgl.HGL) (*Spec, error) {
 		stage:   dict.One("stage"),
 		markers: markers,
 	}
+	return &config, nil
+}
 
-	// vars (optional)
-
+func buildVars(h hgl.HGL) map[string][]string {
 	var vars map[string][]string
-	sec, ok = h["vars"]
+	sec, ok := h["vars"]
 	if ok {
-		dict = sec.(*hgl.DictSection)
+		dict := sec.(*hgl.DictSection)
 		vars = dict.Map()
 	}
+	return vars
+}
 
-	// rewrite
-
-	sec, ok = h["rewrite"]
+func buildRewrite(h hgl.HGL) ([]hgl.Pair, error) {
+	sec, ok := h["rewrite"]
 	if !ok {
 		return nil, errors.New("'rewrite' section required")
 	}
-	list = sec.(*hgl.ListSection)
+	list := sec.(*hgl.ListSection)
 	rewrite := list.Array()
+	return rewrite, nil
+}
 
-	// hangulize
-
-	sec, ok = h["hangulize"]
+func buildHangulize(h hgl.HGL) ([]hgl.Pair, error) {
+	sec, ok := h["hangulize"]
 	if !ok {
 		return nil, errors.New("'hangulize' section required")
 	}
-	list = sec.(*hgl.ListSection)
-	hangulize := list.Array()
+	list := sec.(*hgl.ListSection)
+	rewrite := list.Array()
+	return rewrite, nil
+}
 
-	// test (optional)
-
+func buildTest(h hgl.HGL) []hgl.Pair {
 	var test []hgl.Pair
-	sec, ok = h["test"]
+	sec, ok := h["test"]
 	if ok {
-		list = sec.(*hgl.ListSection)
+		list := sec.(*hgl.ListSection)
 		test = list.Array()
 	}
-
-	spec := Spec{
-		lang,
-		config,
-		vars,
-		rewrite,
-		hangulize,
-		test,
-	}
-	return &spec, nil
+	return test
 }
