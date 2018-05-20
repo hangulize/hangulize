ba6bd1b32f47b1f698303c6ef14b319d7315ebe7
Heungsub Lee <sub@subl.ee>
2018-05-20 21:53:30 +0900

    Split tokens.go

--- hgl/scanner.go
+++ hgl/scanner.go
@@ -3,66 +3,10 @@ package hgl
 import (
 	"bufio"
 	"bytes"
-	"fmt"
 	"io"
+	"strings"
 	"unicode"
 )
-import ()
-
-// Token represents a meaningful string in HGL format.
-type Token int
-
-const (
-	// Illegal represents any string not matched with legal tokens.
-	Illegal Token = iota
-
-	// EOF represents the end-of-file.
-	EOF
-
-	// Newline means only "\n".  HGL is a line-sensitive format.
-	Newline
-
-	// Space represents any of whitespace characters except "\n".
-	Space
-
-	// String represents a text literal.
-	String
-
-	// Comment represents a comment literal including initial "#".
-	Comment
-
-	// Colon means only ":".
-	Colon
-
-	// Comma means only ",".
-	Comma
-
-	// Equal means only "=".
-	Equal
-
-	// Arrow means only "->".
-	Arrow
-)
-
-var tokenNames = map[Token]string{
-	Illegal: `Illegal`,
-	EOF:     `EOF`,
-	Newline: `Newline`,
-	Space:   `Space`,
-	String:  `String`,
-	Comment: `Comment`,
-	Colon:   `Colon`,
-	Comma:   `Comma`,
-	Equal:   `Equal`,
-	Arrow:   `Arrow`,
-}
-
-// FormatTokenLiteral formats return value (token, literal) from Scan() as a
-// human-readable string.
-func FormatTokenLiteral(token Token, literal string) string {
-	tokenName := tokenNames[token]
-	return fmt.Sprintf(`<%s: %#v>`, tokenName, literal)
-}
 
 // Scanner reads a bytes buffer and pops the peak token and literal.
 type Scanner struct {
---
+++ hgl/tokens.go
@@ -0,0 +1,60 @@
+package hgl
+
+import (
+	"fmt"
+)
+
+// Token represents a meaningful string in HGL format.
+type Token int
+
+const (
+	// Illegal represents any string not matched with legal tokens.
+	Illegal Token = iota
+
+	// EOF represents the end-of-file.
+	EOF
+
+	// Space represents any of whitespace characters except "\n".
+	Space
+
+	// Comment represents a comment content excluding initial "#".
+	Comment
+
+	// Newline means only "\n".  HGL is a line-sensitive format.
+	Newline
+
+	// String represents a text literal.
+	String
+
+	// Colon means only ":".
+	Colon
+
+	// Comma means only ",".
+	Comma
+
+	// Equal means only "=".
+	Equal
+
+	// Arrow means only "->".
+	Arrow
+)
+
+var tokenNames = map[Token]string{
+	Illegal: `Illegal`,
+	EOF:     `EOF`,
+	Space:   `Space`,
+	Comment: `Comment`,
+	Newline: `Newline`,
+	String:  `String`,
+	Colon:   `Colon`,
+	Comma:   `Comma`,
+	Equal:   `Equal`,
+	Arrow:   `Arrow`,
+}
+
+// FormatTokenLiteral formats return value (token, literal) from Scan() as a
+// human-readable string.
+func FormatTokenLiteral(token Token, literal string) string {
+	tokenName := tokenNames[token]
+	return fmt.Sprintf(`<%s: %#v>`, tokenName, literal)
+}
