3f5e17137252fc93b208a91a3516a9ba2a0154b8
Heungsub Lee <sub@subl.ee>
2018-05-20 23:49:06 +0900

    Split parser test cases

--- hgl/parser_test.go
+++ hgl/parser_test.go
@@ -7,19 +7,41 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParse(t *testing.T) {
-	src := strings.TrimSpace(`
+func newParser(src string) *Parser {
+	return NewParser(strings.NewReader(strings.TrimSpace(src)))
+}
+
+func TestParseSinglePairList(t *testing.T) {
+	p := newParser(`
 	foo:
 		# 코멘트
-		hello = "world"
+		hello -> world
 	`)
-	p := NewParser(strings.NewReader(src))
 
 	hgl, err := p.Parse()
 	if err != nil {
 		t.Error(err)
 	}
 
-	foo := hgl["foo"].(Dict)
+	foo := hgl["foo"].(*ListSection).Array()
+
+	assert.Equal(t, "hello", foo[0].Left())
+	assert.Equal(t, []string{"world"}, foo[0].Right())
+}
+
+func TestParseSinglePairDict(t *testing.T) {
+	p := newParser(`
+	foo:
+		# 코멘트
+		hello = "world"
+	`)
+
+	hgl, err := p.Parse()
+	if err != nil {
+		t.Error(err)
+	}
+
+	foo := hgl["foo"].(*DictSection).Map()
+
 	assert.Equal(t, []string{"world"}, foo["hello"])
 }
