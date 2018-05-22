2b5e52549a7fde7c960f92c8bb532ab3cf69de65
Heungsub Lee <sub@subl.ee>
2018-05-22 14:01:25 +0900

    Implement assertMatch helper

--- hangulize/pattern_test.go
+++ hangulize/pattern_test.go
@@ -34,6 +34,32 @@ func compile(expr string) *Pattern {
 	return CompilePattern(expr, spec)
 }
 
+const ok = "ok"
+const no = ""
+
+// assertMatch is a helper to test a pattern with multiple examples:
+//
+//  p := compile("foo")
+//  assertMatch(t, p, []string{
+//    ok, "foo",
+//    ok, "foobar",
+//    no, "bar",
+//  })
+//
+func assertMatch(t *testing.T, p *Pattern, scenario []string) {
+	for i := 0; i < len(scenario); i += 2 {
+		shouldMatch := scenario[i] == ok
+		text := scenario[i+1]
+
+		matched := p.Match(text)
+		if shouldMatch {
+			assert.NotEmptyf(t, matched, "%s must match with %#v", p, text)
+		} else {
+			assert.Emptyf(t, matched, "%s must not match with  %#v", p, text)
+		}
+	}
+}
+
 func TestMacro(t *testing.T) {
 	p = compile("@")
 	assert.Equal(t, "(a|e|i|o|u)", p.reExpr)
