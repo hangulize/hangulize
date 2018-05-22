dba6e842f9c0b2794d71dd2d998708f42394b374
Heungsub Lee <sub@subl.ee>
2018-05-23 02:47:42 +0900

    Write docs

--- hangulize/event.go
+++ hangulize/event.go
@@ -1,12 +1,14 @@
 package hangulize
 
-// Event is used for tracing of Hangulize pipeline internal.
+// Event is emitted when a replacement occurs.  It is used for tracing of
+// Hangulize pipeline internal.
 type Event struct {
 	from string
 	to   string
 	why  string
 }
 
+// Emit a replacement event to be traced.
 func event(ch chan<- Event, from string, to string, why string) {
 	if ch == nil {
 		return
