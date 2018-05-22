7d1ae60686c0336e7b4d30dd0c964ba1254aa82a
Heungsub Lee <sub@subl.ee>
2018-05-22 13:35:04 +0900

    Implement positive lookaround expansion

--- hangulize/pattern.go
+++ hangulize/pattern.go
@@ -1,33 +1,54 @@
 package hangulize
 
 import (
+	"fmt"
 	"regexp"
 	"strings"
 )
 
 // Pattern is a domain-specific regular expression dialect for Hangulize.
 type Pattern struct {
-	expr   string
-	reExpr string
-	re     regexp.Regexp
+	expr string
+
+	re  *regexp.Regexp // positive regexp
+	neg *regexp.Regexp // negative regexp
 }
 
 // CompilePattern compiles an Pattern pattern for the given language spec.
 func CompilePattern(expr string, spec *Spec) *Pattern {
 	reExpr := expr
 
+	// spec dependent
 	reExpr = expandMacros(reExpr, spec)
 	reExpr = expandVars(reExpr, spec)
 
-	return &Pattern{expr, reExpr, *regexp.MustCompile(reExpr)}
+	// lookaround
+	reExpr, negExpr := expandLookaround(reExpr)
+
+	re := regexp.MustCompile(reExpr)
+	neg := regexp.MustCompile(negExpr)
+
+	return &Pattern{expr, re, neg}
 }
 
 var (
-	reVar *regexp.Regexp
+	reVar        *regexp.Regexp
+	reLookbehind *regexp.Regexp
+	reLookahead  *regexp.Regexp
+	reZeroWidth  *regexp.Regexp
 )
 
 func init() {
-	reVar = regexp.MustCompile("<.+?>")
+	reVar = regexp.MustCompile(`<.+?>`)
+
+	var (
+		zeroWidth = `\{(.+?)\}` // {...}
+		leftEdge  = `^(\^*)`    // "^", "^^", or empty start
+		rightEdge = `(\$*)$`    // "$", "$$", or empty end
+	)
+	reLookbehind = regexp.MustCompile(leftEdge + zeroWidth)
+	reLookahead = regexp.MustCompile(zeroWidth + rightEdge)
+	reZeroWidth = regexp.MustCompile(zeroWidth)
 }
 
 func expandMacros(reExpr string, spec *Spec) string {
@@ -59,6 +80,67 @@ func expandVars(reExpr string, spec *Spec) string {
 		}
 		return "(" + strings.Join(escapedVals, "|") + ")"
 	})
+}
+
+func expandLookaround(reExpr string) (string, string) {
+	var loc []int
+
+	posExpr := reExpr
+	negExpr := ""
+
+	// TODO(sublee): negative lookaround
+	// TODO(sublee): edge specialization
+
+	loc = reLookbehind.FindStringSubmatchIndex(posExpr)
+	if len(loc) == 6 {
+		// lookbehind found
+
+		// ^{han}gul
+		// │  │   └─ other
+		// │  └─ look
+		// └─ edge
+		edgeExpr := posExpr[loc[2]:loc[3]]
+		lookExpr := posExpr[loc[4]:loc[5]]
+		otherExpr := posExpr[loc[1]:]
+
+		// Replace lookbehind with 2 parentheses:
+		// (^)(han)gul
+		posExpr = fmt.Sprintf(`(%s)(%s)%s`, edgeExpr, lookExpr, otherExpr)
+	} else {
+		// Prepend empty 2 parentheses.
+		posExpr = "()()" + posExpr
+	}
+
+	loc = reLookahead.FindStringSubmatchIndex(posExpr)
+	if len(loc) == 6 {
+		// lookahead found
+
+		// han{gul}$
+		//  │   │  └─ edge
+		//  │   └─ look
+		//  └─ other
+		otherExpr := posExpr[:loc[0]]
+		lookExpr := posExpr[loc[2]:loc[3]]
+		edgeExpr := posExpr[loc[4]:loc[5]]
+
+		// Replace lookahead with 2 parentheses:
+		// han(gul)($)
+		posExpr = fmt.Sprintf(`%s(%s)(%s)`, otherExpr, lookExpr, edgeExpr)
+	} else {
+		// Append empty 2 parentheses.
+		posExpr = posExpr + "()()"
+	}
+
+	// Find remaining zero-width groups.
+	if reZeroWidth.MatchString(posExpr) {
+		panic(fmt.Errorf("zero-width group found in middle: %#v", reExpr))
+	}
+
+	if len(negExpr) == 0 {
+		negExpr = "$^" // never match
+	}
+
+	return posExpr, negExpr
 }
 
 // func CompileRewrite(reExpr string, replace func(string) string) Rewrite {
