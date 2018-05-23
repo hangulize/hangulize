42c30ef5035dd7f4113adfe483fd61e2735c7bb5
Heungsub Lee <sub@subl.ee>
2018-05-23 22:37:42 +0900

    Add test case for var to var rewrite

---
+++ hangulize/rewrite_test.go
@@ -0,0 +1,16 @@
+package hangulize
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func rewrite(word string, fromExpr string, toExpr string) string {
+	r := Rule{from: compile(fromExpr), to: []string{toExpr}}
+	return r.Rewrite(word)
+}
+
+func TestVarToVar(t *testing.T) {
+	assert.Equal(t, "d", rewrite("a", "<abc>", "<def>"))
+}
