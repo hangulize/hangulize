a89b571995eb998cc0aa415a9f09c8b6587c602c
Heungsub Lee <sub@subl.ee>
2018-05-25 02:30:59 +0900

    PoC: Pass Spanish

--- hangulize/normalize.go
+++ hangulize/normalize.go
@@ -9,12 +9,32 @@ import (
 func NormalizeRoman(word string) string {
 	var buf strings.Builder
 
+	text := []rune(word)
+
 	var iter norm.Iter
 	iter.InitString(norm.NFD, word)
 
+	i := 0
 	for !iter.Done() {
 		bin := iter.Next()
-		buf.WriteByte(bin[0])
+
+		// POC(sublee): Spanish
+		switch string(text[i]) {
+		case "Ñ":
+			buf.WriteString("ñ")
+		case "ñ":
+			buf.WriteString("ñ")
+		case "Ǘ":
+			buf.WriteString("ü")
+		case "ü":
+			buf.WriteString("ü")
+		case "Ü":
+			buf.WriteString("ü")
+		default:
+			buf.WriteByte(bin[0])
+		}
+
+		i++
 	}
 
 	return strings.ToLower(buf.String())
