64536135aa2f3ed45926e7e9c0ac4a3832f8ee69
Heungsub Lee <sub@subl.ee>
2018-05-24 23:56:24 +0900

    More var-to-var cases

--- hangulize/rewrite_test.go
+++ hangulize/rewrite_test.go
@@ -13,6 +13,9 @@ func rewrite(word string, fromExpr string, toExpr string) string {
 
 func TestVarToVar(t *testing.T) {
 	assert.Equal(t, "d", rewrite("a", "<abc>", "<def>"))
+	assert.Equal(t, "def", rewrite("abc", "<abc>", "<def>"))
+	assert.Equal(t, "fde", rewrite("cab", "<abc>", "<def>"))
+	assert.Equal(t, "XfdeX", rewrite("XcabX", "<abc>", "<def>"))
 }
 
 func TestCaret(t *testing.T) {
