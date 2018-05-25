2b435032a380582da95db515120bdef0dad64cc5
Heungsub Lee <sub@subl.ee>
2018-05-26 01:53:56 +0900

    Make TestLang as subtests

---
+++ hangulize/hangulize_test.go
@@ -0,0 +1,52 @@
+package hangulize
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLang(t *testing.T) {
+	for _, lang := range ListLangs() {
+		t.Run(lang, func(t *testing.T) {
+			spec, ok := LoadSpec(lang)
+
+			assert.Truef(t, ok, `failed to load "%s" spec`, lang)
+
+			h := NewHangulizer(spec)
+
+			for _, testCase := range spec.Test {
+				loanword := testCase.Left()
+				expected := testCase.Right()[0]
+
+				ch := make(chan Trace, 1000)
+				got := h.HangulizeTrace(loanword, ch)
+
+				if got == expected {
+					continue
+				}
+
+				// Trace result to understand the failure reason.
+				f := bytes.NewBufferString("")
+				hr := strings.Repeat("-", 30)
+
+				// Render failure message.
+				fmt.Fprintln(f, hr)
+				fmt.Fprintf(f, `lang: "%s"`, lang)
+				fmt.Fprintln(f)
+				fmt.Fprintf(f, `word: %#v`, loanword)
+				fmt.Fprintln(f)
+				fmt.Fprintln(f, hr)
+				for e := range ch {
+					fmt.Fprintln(f, e.String())
+				}
+				fmt.Fprintln(f, hr)
+
+				assert.Equal(t, expected, got, f.String())
+			}
+		})
+	}
+}
