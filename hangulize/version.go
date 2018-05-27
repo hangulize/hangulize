96d5541a6700f394f19b1cfc3535eb27e1bbbf84
Heungsub Lee <sub@subl.ee>
2018-05-28 02:16:45 +0900

    Bump version: 0.0.3

--- hangulize/version.go
+++ hangulize/version.go
@@ -3,4 +3,4 @@ package hangulize
 // Version is the version number of Hangulize 2 package.  Even though
 // "Hangulize 2" ends with "2", the version starts from "0.0.0".  The version
 // follows Semantic Versioning 2.0.0.
-const Version = "0.0.2"
+const Version = "0.0.3"
