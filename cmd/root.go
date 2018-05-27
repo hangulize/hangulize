724efeee7b0bf1d32b2b8c7541f9cd82a5d76aa5
Heungsub Lee <sub@subl.ee>
2018-05-27 21:05:25 +0900

    hangulize2 LANG WORD to hangulize a word

--- cmd/root.go
+++ cmd/root.go
@@ -5,11 +5,27 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/sublee/hangulize2/hangulize"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "hangulize2",
+	Use:   "hangulize2 LANG WORD",
 	Short: "Hangulize 2 tools",
+
+	Args: cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		lang := args[0]
+		word := args[1]
+
+		spec, ok := hangulize.LoadSpec(lang)
+		if !ok {
+			fmt.Println("Lang not supported:", lang)
+			os.Exit(1)
+		}
+
+		h := hangulize.NewHangulizer(spec)
+		fmt.Println(h.Hangulize(word))
+	},
 }
 
 func Execute() {
