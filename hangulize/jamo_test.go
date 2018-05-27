56d838da94f385ee0749b8426d53bb2cdbb17358
Heungsub Lee <sub@subl.ee>
2018-05-27 20:46:05 +0900

    Make benchmark for ComposeHangul()

--- hangulize/jamo_test.go
+++ hangulize/jamo_test.go
@@ -10,3 +10,9 @@ func TestComposeHangul(t *testing.T) {
 	assert.Equal(t, "한글", ComposeHangul("ㅎㅏ-ㄴㄱㅡ-ㄹ"))
 	assert.Equal(t, "낑깡", ComposeHangul("ㄲㅣ-ㅇㄲㅏ-ㅇ"))
 }
+
+func BenchmarkComposeHangul(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ComposeHangul("ㅎㅏ-ㄴㄱㅡ-ㄹ")
+	}
+}
