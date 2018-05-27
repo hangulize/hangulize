8507b0abd242e9d543189d7784222b0980d3d8b6
Heungsub Lee <sub@subl.ee>
2018-05-28 00:56:53 +0900

    ComposeHangul supports composed Hangul syllable

--- hangulize/jamo_test.go
+++ hangulize/jamo_test.go
@@ -11,6 +11,11 @@ func TestComposeHangul(t *testing.T) {
 	assert.Equal(t, "낑깡", ComposeHangul("ㄲㅣ-ㅇㄲㅏ-ㅇ"))
 }
 
+func TestComposeHangulOnComposed(t *testing.T) {
+	assert.Equal(t, "한글", ComposeHangul("한글"))
+	assert.Equal(t, "한글라이즈", ComposeHangul("하-ㄴ글ㄹㅏ이ㅈ"))
+}
+
 func BenchmarkComposeHangul(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		ComposeHangul("ㅎㅏ-ㄴㄱㅡ-ㄹ")
