c71797e15cf35cdbf8260e60837b785ce705ca77
Heungsub Lee <sub@subl.ee>
2018-05-28 02:01:31 +0900

    Expose __hangulize__ to hangulize.js

--- js/main.go
+++ js/main.go
@@ -8,4 +8,14 @@ import (
 
 func main() {
 	js.Global.Set("hangulize", hangulize.Hangulize)
+	js.Global.Set("__hangulize__", map[string]interface{}{
+		"Hangulize": hangulize.Hangulize,
+		"Version":   hangulize.Version,
+
+		"ListLangs":     hangulize.ListLangs,
+		"LoadSpec":      hangulize.LoadSpec,
+		"NewHangulizer": hangulize.NewHangulizer,
+
+		"ComposeHangul": hangulize.ComposeHangul,
+	})
 }
