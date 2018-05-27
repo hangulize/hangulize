6c38ec26faa432e5c8107508424e8797178e3ff0
Heungsub Lee <sub@subl.ee>
2018-05-27 20:38:36 +0900

    Recover normalize_test.go

--- hangulize/normalize_test.go
+++ hangulize/normalize_test.go
@@ -1,14 +1,23 @@
 package hangulize
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	"github.com/stretchr/testify/assert"
-// )
+	"github.com/stretchr/testify/assert"
+)
 
-// func TestNormalizeRoman(t *testing.T) {
-// 	assert.Equal(t, "hello", NormalizeRoman("Hello", nil))
-// 	assert.Equal(t, "cafe", NormalizeRoman("Café", nil))
-// 	assert.Equal(t, "melee", NormalizeRoman("Mêlée", nil))
-// 	assert.Equal(t, " cafe, hi! ", NormalizeRoman(" Café, Hi! ", nil))
-// }
+func TestNormalizeRoman(t *testing.T) {
+	roman := &RomanNormalizer{}
+
+	assert.Equal(t, "hello", Normalize("Hello", roman, nil))
+	assert.Equal(t, "cafe", Normalize("Café", roman, nil))
+	assert.Equal(t, "melee", Normalize("Mêlée", roman, nil))
+	assert.Equal(t, " cafe, hi! ", Normalize(" Café, Hi! ", roman, nil))
+}
+
+func TestNormalizeKana(t *testing.T) {
+	kana := &KanaNormalizer{}
+
+	assert.Equal(t, "ア", Normalize("あ", kana, nil))
+	assert.Equal(t, "ァ", Normalize("ぁ", kana, nil))
+}
