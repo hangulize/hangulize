455098e5f99172c7b4a1abeb4574ea6d22667e31
Heungsub Lee <sub@subl.ee>
2018-05-28 10:00:21 +0900

    Adjust column widths

--- cmd/ls.go
+++ cmd/ls.go
@@ -16,7 +16,7 @@ var lsCmd = &cobra.Command{
 	Use:   "ls",
 	Short: "List of bundled lang specs",
 	Run: func(cmd *cobra.Command, args []string) {
-		template := "%-8s %-8s %-20s %-20s\n"
+		template := "%-8s %-8s %-24s %-24s\n"
 
 		fmt.Printf(template, "LANG", "STAGE", "ENG", "KOR")
 
