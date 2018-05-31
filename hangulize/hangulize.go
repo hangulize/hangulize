917df989f0fe267b33671dc65b46f37ec1cca877
Heungsub Lee <sub@subl.ee>
2018-05-31 23:22:43 +0900

    Revert "Rename "rewrite" -> "quantize""
    This reverts commit e8934b86b13fb0a8f1dbe0a6dd72ffca75a83c81.

--- hangulize/hangulize.go
+++ hangulize/hangulize.go
@@ -18,21 +18,21 @@ feature improvements.
 Pipeline
 
 When we transcribe, the word goes through Hangulize's procedural pipeline.  The
-pipeline has 5 steps: "Normalize", "Group", "Quantize", "Transcribe", and
+pipeline has 5 steps: "Normalize", "Group", "Rewrite", "Transcribe", and
 "Compose Hangul".  If we can transcribe "Hello!" in English into "헬로!"
 (actually, English is not supported yet), the pipeline world work like:
 
 	0. Input       "Hello!"
 	1. Normalize   "hello!"
 	2. Group       "hello", "!"
-	3. Quantize    "heˈlō", "!"
+	3. Rewrite     "heˈlō", "!"
 	4. Transcribe  "ㅎㅔ-ㄹㄹㅗ", "!"
 	5. Compose H.  "헬로!"
 
 The "1. Normalize" step eliminates letter case from the word to make the next
 steps work easier.  The "2. Group" step groups letters by their meaningness
 into subwords.  Meaningful letter is the letter which appears in the
-quantize/transcribe rules.  The "3. Quantize" step minimizes the gap between
+rewrite/transcribe rules.  The "3. Rewrite" step minimizes the gap between
 pronunciation and spelling.  The "4. Transcribe" step determines Hangul
 spelling for the pronunciation.  Finally, the  "5. Compose Hangul" step
 converts decomposed Jamo phonemes to composed Hangul syllables.
@@ -56,9 +56,9 @@ Then write about yourself and the stage of this spec:
 	    author = "John Doe <john@example.com>"
 	    stage  = "draft"
 
-We will write many patterns in quantize/transcribe rules soon.  Some
-expressions may appear many times annoyingly.  To not repeat ourselves, we can
-use variables and macros.
+We will write many patterns in rewrite/transcribe rules soon.  Some expressions
+may appear many times annoyingly.  To not repeat ourselves, we can use
+variables and macros.
 
 A variable is a combination of letters.  Variable in pattern will match with
 one of the letters.  Variable "foo" can be referenced with "<foo>" in the
@@ -73,12 +73,12 @@ is the common macro for "<vowels>" variable:
 	macros:
 	    "@" = "<vowels>"
 
-Now we can write "quantize" rules.  There are Pattern and RPattern.  Pattern
+Now we can write "rewrite" rules.  There are Pattern and RPattern.  Pattern
 matches with letters in a word.  RPattern represents how the matched letters
 should be replaced.  A replaced word by a rule would become as the input for
 the next rule:
 
-	quantize:
+	rewrite:
 	    "^gli$"   -> "li"
 	    "^gli{@}" -> "li"
 	    "{@}gli"  -> "li"
@@ -88,8 +88,8 @@ Pattern is based on Regular Expression but it has it's own custom syntax.  We
 call it "HRE" which means "Hangulize-specific Regular Expression".  For the
 detail, see the documentation of Pattern.
 
-"transcribe" rules are exactly same with "quantize" rules.  But it's RPatterns
-represent Hangul Jamo phonemes.  In contrast to "quantize", a replaced word
+"transcribe" rules are exactly same with "rewrite" rules.  But it's RPatterns
+represent Hangul Jamo phonemes.  In contrast to "rewrite", a replaced word
 won't become as the input for the next rules:
 
 	transcribe:
