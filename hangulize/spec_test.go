71605ae550772fc95c3cbfe454075cb80aee2a14
Heungsub Lee <sub@subl.ee>
2018-06-01 08:56:27 +0900

    Make test case for Spec.Source

--- hangulize/spec_test.go
+++ hangulize/spec_test.go
@@ -10,3 +10,14 @@ func TestEmptySpec(t *testing.T) {
 	spec := mustParseSpec(``)
 	assert.Equal(t, "", spec.Lang.ID)
 }
+
+func TestSpecSource(t *testing.T) {
+	spec := mustParseSpec(`
+	rewrite:
+		"hello" -> "world"
+	`)
+	assert.Equal(t, `
+	rewrite:
+		"hello" -> "world"
+	`, spec.Source)
+}
