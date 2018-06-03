2cd7341513f1496518fdde6257f9b3f84d17ccb6
Heungsub Lee <sub@subl.ee>
2018-06-04 02:49:29 +0900

    Export things into hangulize JS function

--- js/main.go
+++ js/main.go
@@ -1,21 +1,50 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/gopherjs/gopherjs/js"
 
 	"github.com/sublee/hangulize2/hangulize"
 )
 
+// Langs is the preloaded language informations.
+var Langs = make(map[string]hangulize.Language)
+
+func init() {
+	for _, lang := range hangulize.ListLangs() {
+		spec, _ := hangulize.LoadSpec(lang)
+		Langs[lang] = spec.Lang
+	}
+}
+
 func main() {
-	js.Global.Set("hangulize", hangulize.Hangulize)
-	js.Global.Set("__hangulize__", map[string]interface{}{
+	exports := map[string]interface{}{
 		"Hangulize": hangulize.Hangulize,
 		"Version":   hangulize.Version,
+		"Langs":     Langs,
 
-		"ListLangs":     hangulize.ListLangs,
-		"LoadSpec":      hangulize.LoadSpec,
-		"NewHangulizer": hangulize.NewHangulizer,
+		"LoadSpec": hangulize.LoadSpec,
 
-		"ComposeHangul": hangulize.ComposeHangul,
-	})
+		"ParseSpec": func(source string) *js.Object {
+			r := strings.NewReader(source)
+			spec, _ := hangulize.ParseSpec(r)
+			return js.MakeWrapper(spec)
+		},
+
+		"NewHangulizer": func(spec *hangulize.Spec) *js.Object {
+			h := hangulize.NewHangulizer(spec)
+			return js.MakeWrapper(h)
+		},
+	}
+
+	js.Global.Set("hangulize", hangulize.Hangulize)
+
+	for attr, val := range exports {
+		js.Global.Get("hangulize").Set(attr, val)
+	}
+
+	if js.Module != js.Undefined {
+		js.Module.Set("exports", exports)
+	}
 }
