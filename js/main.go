cdecc0ed3e3380b20b9d6348437ba6f3dd127010
Heungsub Lee <sub@subl.ee>
2018-06-05 00:00:53 +0900

    Pack spec for JS use

--- js/main.go
+++ js/main.go
@@ -8,32 +8,80 @@ import (
 	"github.com/sublee/hangulize2/hangulize"
 )
 
-// Langs is the preloaded language informations.
-var Langs = make(map[string]hangulize.Language)
+// packSpec packs a spec into {"spec": the-spec, "info": {"lang":, "config":,
+// "test":, "source": ...}}.  It exposes some information
+// to be used in JavaScript-side.
+func packSpec(s *hangulize.Spec) *js.Object {
+	// Append only lang, config, test, source.
+	info := js.Global.Get("Object").New()
+
+	lang := js.Global.Get("Object").New()
+	lang.Set("id", s.Lang.ID)
+	lang.Set("codes", s.Lang.Codes)
+	lang.Set("english", s.Lang.English)
+	lang.Set("korean", s.Lang.Korean)
+	lang.Set("script", s.Lang.Script)
+
+	config := js.Global.Get("Object").New()
+	config.Set("authors", s.Config.Authors)
+	config.Set("stage", s.Config.Stage)
+
+	test := js.Global.Get("Object").New()
+	for i, pair := range s.Test {
+		o := js.Global.Get("Object").New()
+		o.Set("word", pair.Left())
+		o.Set("transcribed", pair.Right()[0])
+		test.SetIndex(i, &o)
+	}
+
+	info.Set("lang", lang)
+	info.Set("config", config)
+	info.Set("test", test)
+	info.Set("source", s.Source)
+
+	// Result
+	o := js.Global.Get("Object").New()
+	o.Set("spec", js.MakeWrapper(s))
+	o.Set("info", info)
+	return o
+}
+
+var specs = make(map[string]*js.Object)
 
 func init() {
 	for _, lang := range hangulize.ListLangs() {
 		spec, _ := hangulize.LoadSpec(lang)
-		Langs[lang] = spec.Lang
+		specs[lang] = packSpec(spec)
 	}
 }
 
 func main() {
 	exports := map[string]interface{}{
-		"Hangulize": hangulize.Hangulize,
-		"Version":   hangulize.Version,
-		"Langs":     Langs,
+		"hangulize": hangulize.Hangulize,
+		"version":   hangulize.Version,
 
-		"LoadSpec": hangulize.LoadSpec,
+		"specs": specs,
 
-		"ParseSpec": func(source string) *js.Object {
-			r := strings.NewReader(source)
-			spec, _ := hangulize.ParseSpec(r)
-			return js.MakeWrapper(spec)
+		"loadSpec": func(langID string) *js.Object {
+			spec, ok := hangulize.LoadSpec(langID)
+			if !ok {
+				return nil
+			}
+			return packSpec(spec)
 		},
 
-		"NewHangulizer": func(spec *hangulize.Spec) *js.Object {
-			h := hangulize.NewHangulizer(spec)
+		"parseSpec": func(source string) *js.Object {
+			r := strings.NewReader(source)
+			spec, err := hangulize.ParseSpec(r)
+			if err != nil {
+				return nil
+			}
+			return packSpec(spec)
+		},
+
+		"hangulizer": func(spec *js.Object) *js.Object {
+			_spec := spec.Get("spec").Interface().(*hangulize.Spec)
+			h := hangulize.NewHangulizer(_spec)
 			return js.MakeWrapper(h)
 		},
 	}
