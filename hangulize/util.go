1bf0b0c7e0f621b7dcfe407fb654319b611be32c
Heungsub Lee <sub@subl.ee>
2018-06-06 17:35:17 +0900

    Split stringset.go

--- hangulize/util.go
+++ hangulize/util.go
@@ -1,9 +1,7 @@
 package hangulize
 
 import (
-	"fmt"
 	"regexp"
-	"sort"
 	"strings"
 )
 
@@ -83,49 +81,6 @@ func indexOf(val string, vals []string) int {
 		}
 	}
 	return -1
-}
-
-// -----------------------------------------------------------------------------
-
-type stringSet map[string]bool
-
-func (s *stringSet) String() string {
-	return fmt.Sprint(s.Array())
-}
-
-// newStringSet creates a stringSet from the given strings.
-// Duplicated string doesn't occur a failure.
-func newStringSet(strs ...string) stringSet {
-	set := make(stringSet, len(strs))
-	for _, str := range strs {
-		set[str] = true
-	}
-	return set
-}
-
-// Has tests if the string is in the set.
-func (s *stringSet) Has(str string) bool {
-	return (*s)[str]
-}
-
-// HasRune tests if the rune is in the set.
-func (s *stringSet) HasRune(ch rune) bool {
-	return s.Has(string(ch))
-}
-
-// Array returns a []string array containing strings in the set.
-// Each string is unique and ordered in ascending order.
-func (s *stringSet) Array() []string {
-	strings := make([]string, len(*s))
-
-	i := 0
-	for str := range *s {
-		strings[i] = str
-		i++
-	}
-
-	sort.Strings(strings)
-	return strings
 }
 
 // -----------------------------------------------------------------------------
