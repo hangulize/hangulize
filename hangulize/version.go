cf67e3e95f4d4a34a9359def602df36cbf3bcd40
Heungsub Lee <sub@subl.ee>
2018-06-10 23:48:56 +0900

    Bump version: 0.0.7-dev
    [ci skip]

--- hangulize/version.go
+++ hangulize/version.go
@@ -3,4 +3,4 @@ package hangulize
 // Version is the version number of Hangulize 2 package. Even though
 // "Hangulize 2" ends with "2", the version starts from "0.0.0". The version
 // follows Semantic Versioning 2.0.0.
-const Version = "0.0.6"
+const Version = "0.0.7-dev"
