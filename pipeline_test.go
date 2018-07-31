f4095aea2fe35a1a0e45545a4004f9b2b8c7fafc
Heungsub Lee <sub@subl.ee>
2018-08-01 00:10:59 +0900

    Test localizePuncts

---
+++ pipeline_test.go
@@ -0,0 +1,18 @@
+package hangulize
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLocalizePuncts(t *testing.T) {
+	s := Spec{}
+	s.script = _Kana{}
+	h := NewHangulizer(&s)
+	p := pipeline{h, nil}
+
+	assert.Equal(t, "foo.", p.localizePuncts("foo。"))
+	assert.Equal(t, ", foo, bar,", p.localizePuncts("、foo、bar、"))
+	assert.Equal(t, "'foo' bar", p.localizePuncts("「foo」bar"))
+}
