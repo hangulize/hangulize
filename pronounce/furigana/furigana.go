0e5ac967456e98a0a360da70f76d9823b205b5b7
Heungsub Lee <sub@subl.ee>
2018-08-01 02:30:34 +0900

    Refactor analyze() with Typewriter

--- pronounce/furigana/furigana.go
+++ pronounce/furigana/furigana.go
@@ -6,10 +6,6 @@ pronounced. This pronouncer uses IPADIC in Kagome to analyze Kanji.
 package furigana
 
 import (
-	"bytes"
-	"strings"
-	"unicode/utf8"
-
 	kagome "github.com/ikawaha/kagome/tokenizer"
 )
 
@@ -52,107 +48,11 @@ func (p *furiganaPronouncer) Pronounce(word string) string {
 	// Don't initialize the Kagome tokenizer if there's no Kanji because
 	// Kagome is expensive.
 	if kanjiFound {
-		word = p.analyze(word)
+		tokens := p.Kagome().Tokenize(word)
+		tw := newTypewriter(tokens)
+		word = tw.Typewrite()
 	}
 
 	word = repeatKana(word)
 	return word
-}
-
-type chunk struct {
-	s   string
-	sep bool
-}
-
-func (p *furiganaPronouncer) analyze(word string) string {
-	var chunks []chunk
-
-	skipNextSep := false
-
-	for _, tok := range p.Kagome().Tokenize(word) {
-		switch tok.Class {
-
-		case kagome.KNOWN:
-			fs := tok.Features()
-			// 0: part-of-speech
-			// 1: sub-class 1
-			// 2: sub-class 2
-			// 3: sub-class 3
-			// 4: inflection
-			// 5: conjugation
-			// 6: root-form
-			// 7: reading
-			// 8: pronunciation
-
-			var (
-				part    = fs[0]
-				class   = fs[1]
-				reading = fs[7]
-			)
-
-			// Disobey the Boolean gen for comments.
-			var sep bool
-			if skipNextSep {
-				// The previous chunks was a custom separator.
-				// Don't prepend new one.
-				sep = false
-				skipNextSep = false
-			} else if class == "固有名詞" {
-				// Proper noun require a prior separator.
-				sep = true
-			} else {
-				// Prepend a separator before ア-オ. Because it should not be
-				// treated as a long vowel.
-				first, _ := utf8.DecodeRuneInString(reading)
-				sep = first%2 == 0 && in(first, 'ア', 'オ')
-			}
-
-			c := chunk{reading, sep}
-			chunks = append(chunks, c)
-
-			if part == "フィラー" {
-				// Just fillers should be merged with the next chunk.
-				skipNextSep = true
-			} else if part == "記号" {
-				// Symbols are also separators.
-				skipNextSep = true
-			}
-
-		case kagome.UNKNOWN:
-			surf := tok.Surface
-			isSpace := strings.TrimSpace(surf) == ""
-
-			var c *chunk
-			if isSpace {
-				// Whitespace is used as the sep.
-				c = &chunk{surf, false}
-				skipNextSep = true
-			} else {
-				c = &chunk{surf, !skipNextSep}
-				skipNextSep = false
-			}
-			chunks = append(chunks, *c)
-
-		default:
-			continue
-
-		}
-	}
-
-	var buf bytes.Buffer
-
-	skipSep := true
-	for _, c := range chunks {
-		if c.sep {
-			if skipSep {
-				skipSep = false
-			} else {
-				// Separate this chunk by Nakaguro.
-				buf.WriteRune('・')
-			}
-		}
-		buf.WriteString(c.s)
-	}
-
-	return buf.String()
 }
--- pronounce/furigana/furigana_test.go
+++ pronounce/furigana/furigana_test.go
@@ -17,13 +17,9 @@ func TestKana(t *testing.T) {
 	assert.Equal(t, "ゲーム", P.Pronounce("ゲーム"))
 }
 
-func TestPersons(t *testing.T) {
-	assert.Equal(t, "シンカイ・マコト", P.Pronounce("新海誠"))
-	assert.Equal(t, "マツモト・ユキヒロ", P.Pronounce("松本行弘"))
-}
-
-func TestPlaces(t *testing.T) {
-	assert.Equal(t, "トウキョウ", P.Pronounce("東京"))
+func TestPersonNames(t *testing.T) {
+	assert.Equal(t, "シンカイ マコト", P.Pronounce("新海誠"))
+	assert.Equal(t, "マツモト ユキヒロ", P.Pronounce("松本行弘"))
 }
 
 func TestDeduplicateSpaces(t *testing.T) {
@@ -31,12 +27,19 @@ func TestDeduplicateSpaces(t *testing.T) {
 	assert.Equal(t, "マツモト ユキヒロ", P.Pronounce("松本 行弘"))
 }
 
+func TestPlaces(t *testing.T) {
+	assert.Equal(t, "トウキョウ", P.Pronounce("東京"))
+}
+
 func TestRepeatKana(t *testing.T) {
 	assert.Equal(t, "バナナ", P.Pronounce("バナヽ"))
 	assert.Equal(t, "イスズジドウシャ", P.Pronounce("いすゞ自動車"))
 }
 
-func TestNakaguroBetweenProperNouns(t *testing.T) {
-	assert.Equal(t, "カゼノタニ", P.Pronounce("風の谷"))
-	assert.Equal(t, "キムラ・タクヤ", P.Pronounce("木村拓哉"))
+func TestProperNouns(t *testing.T) {
+	assert.Equal(t, "テンクウノシロラピュタ", P.Pronounce("天空の城ラピュタ"))
+}
+
+func TestFiller(t *testing.T) {
+	assert.Equal(t, "アアッメガミサマッ", P.Pronounce("ああっ女神さまっ"))
 }
