6c8b04b46ee9a0a3d30673a4cf3829dc24b79f85
Heungsub Lee <sub@subl.ee>
2018-09-16 18:10:18 +0900

    100% coverage for phonemize/furigana

--- phonemize/furigana/typewriter_test.go
+++ phonemize/furigana/typewriter_test.go
@@ -20,3 +20,21 @@ func TestTypewriterUnknown(t *testing.T) {
 
 	assert.Equal(t, "ホーオー", tw.Typewrite())
 }
+
+func TestTypewriterParticles(t *testing.T) {
+	toks := kagome.New().Tokenize("それはあちへ")
+	tw := newTypewriter(toks)
+
+	// ハ instead of ワ, ヘ instead of エ
+	assert.Equal(t, "ソレハアチヘ", tw.Typewrite())
+}
+
+func TestTypewriterReuse(t *testing.T) {
+	toks := kagome.New().Tokenize("東京")
+	tw := newTypewriter(toks)
+
+	assert.Equal(t, -1, tw.cur)
+	assert.Equal(t, "トーキョー", tw.Typewrite())
+	assert.NotEqual(t, -1, tw.cur)
+	assert.Equal(t, "トーキョー", tw.Typewrite())
+}
