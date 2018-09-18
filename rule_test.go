0acc5f4c9bf7b032c8fcbe636bd183614706febc
Heungsub Lee <sub@subl.ee>
2018-09-19 01:59:14 +0900

    Test Rule.replacements()

--- rule_test.go
+++ rule_test.go
@@ -13,3 +13,16 @@ func TestRuleString(t *testing.T) {
 	r := Rule{p, rp}
 	assert.Equal(t, `"foo" -> "bar"`, r.String())
 }
+
+func TestRuleReplacements(t *testing.T) {
+	p, _ := hre.NewPattern("foo", nil, nil)
+	rp := hre.NewRPattern("bar", nil, nil)
+	r := Rule{p, rp}
+
+	repls := r.replacements("abcfoodef")
+
+	assert.Len(t, repls, 1)
+	assert.Equal(t, 3, repls[0].start)
+	assert.Equal(t, 6, repls[0].stop)
+	assert.Equal(t, "bar", repls[0].word)
+}
