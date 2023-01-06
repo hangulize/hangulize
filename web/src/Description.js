c773d5fd390f49d9cde5467ca193eb99c64cfef9
Heungsub Lee <heungsub@subl.ee>
2023-01-06 17:57:39 +0000

    Update description

--- web/src/Description.js
+++ web/src/Description.js
@@ -1,7 +1,7 @@
 function Description() {
   return (
     <p className="description">
-      한글라이즈는 <a href="https://kornorms.korean.go.kr/regltn/regltnView.do?regltn_code=0003&regltn_no=425" target="_blank" rel="noreferrer">외래어 표기법</a>을 준수하여 외래어 단어를 한글로 <a href="https://ko.wikipedia.org/wiki/%EC%A0%84%EC%82%AC_(%EC%96%B8%EC%96%B4%ED%95%99)" target="_blank" rel="noreferrer">전사</a>하는 도구입니다. 관례적 한글 표기가 자리 잡지 않은 외국 인명이나 지명을 한글로 옮겨적는 데에 활용할 수 있습니다. 모든 결과가 정확하진 않으므로 중요 문건에 활용할 땐 제3의 방법으로 검증하길 권장합니다.
+      한글라이즈는 <a href="https://kornorms.korean.go.kr/regltn/regltnView.do?regltn_code=0003&regltn_no=425" target="_blank" rel="noreferrer">외래어 표기법</a>을 준수하여 외래어 단어를 한글로 <a href="https://ko.wikipedia.org/wiki/%EC%A0%84%EC%82%AC_(%EC%96%B8%EC%96%B4%ED%95%99)" target="_blank" rel="noreferrer">전사</a>하는 도구입니다. 관례적 한글 표기가 자리 잡지 않은 외국 인명이나 지명을 한글로 옮겨적는 데에 활용할 수 있습니다.
     </p>
   )
 }
