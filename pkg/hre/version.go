56e62eb3218ea912ab2ad8c50c762ee907ff441a
Heungsub Lee <heungsub@subl.ee>
2023-01-08 19:43:44 +0000

    Remove version.go

--- pkg/hre/version.go
+++
@@ -1,5 +0,0 @@
-package hre
-
-// Version is the version number of HRE package. The version follows Semantic
-// Versioning 2.0.0.
-const Version = "0.2.2"
--- version.go
+++
@@ -1,5 +0,0 @@
-package hangulize
-
-// Version is the version number of Hangulize package. The version follows
-// Semantic Versioning 2.0.0.
-const Version = "0.3.5"
