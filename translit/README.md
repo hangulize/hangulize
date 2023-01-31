da784c83ad14d3e2fe51e1b5884540d1c3bd478b
Heungsub Lee <heungsub@subl.ee>
2023-02-01 02:28:06 +0900

    Introduce hangulize/translit

---
+++ translit/README.md
@@ -0,0 +1,29 @@
+# Translit
+
+Here are packages that implement the `hangulize.Translit` interface for
+specific methods. Each package provides `T` as the singleton value.
+
+Install all of the standard Translits:
+
+```go
+import "github.com/hangulize/hangulize"
+import "github.com/hangulize/hangulize/translit"
+
+func main() {
+    translit.Install()
+    hangulize.Hangulize("chi", "靑島")
+    hangulize.Hangulize("jpn", "北海道")
+}
+```
+
+Or use a specific Translit to reduce the build size:
+
+```go
+import "github.com/hangulize/hangulize"
+import "github.com/hangulize/hangulize/translit/furigana"
+
+func main() {
+    hangulize.UseTranslit(&furigana.T)
+    hangulize.Hangulize("jpn", "自由ヶ丘")
+}
+```
---
+++ translit/translit.go
@@ -0,0 +1,38 @@
+package translit
+
+import (
+	"github.com/hangulize/hangulize"
+	"github.com/hangulize/hangulize/translit/furigana"
+	"github.com/hangulize/hangulize/translit/pinyin"
+)
+
+// Translits returns the standard Translits.
+func Translits() []hangulize.Translit {
+	ts := []hangulize.Translit{furigana.T, pinyin.T}
+	return ts
+}
+
+// Install imports all of the standard Translits.
+func Install(h ...hangulize.Hangulizer) bool {
+	if len(h) > 1 {
+		panic("usage: translit.Use([hangulizer])")
+	}
+
+	useTranslit := hangulize.UseTranslit
+	unuseTranslit := hangulize.UnuseTranslit
+	if len(h) == 1 {
+		useTranslit = h[0].UseTranslit
+		unuseTranslit = h[0].UnuseTranslit
+	}
+
+	ts := Translits()
+	for i, t := range ts {
+		if ok := useTranslit(t); !ok {
+			for j := 0; j < i; j++ {
+				unuseTranslit(ts[j].Method())
+			}
+			return false
+		}
+	}
+	return true
+}
