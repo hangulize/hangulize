ea34f7bcc3c8711fea1f0af541bf4d363c8b24b7
Heungsub Lee <heungsub@subl.ee>
2023-02-19 00:50:29 +0900

    Map traceFn param of hangulize() on worker API

--- web/src/hangulize/hangulize.worker.ts
+++ web/src/hangulize/hangulize.worker.ts
@@ -5,11 +5,17 @@ import * as Comlink from 'comlink'
 import { runWebAssembly } from './runWebAssembly'
 import type { Specs } from './spec'
 
+export interface HangulizeTrace {
+  step: string
+  word: string
+  why: string
+}
+
 interface WasmHangulize {
-  (lang: string, word: string): Promise<string>
+  (lang: string, word: string, traceFn?: (t: HangulizeTrace) => void): Promise<string>
   version: string
   specs: Specs
-  useTranslit: (method: string, fn: (word: string) => Promise<string>) => void
+  useTranslit: (method: string, fn: (word: string) => Promise<string>) => boolean
 }
 
 declare global {
@@ -19,16 +25,16 @@ declare global {
 const ready = runWebAssembly(new URL('hangulize.wasm', import.meta.url))
 
 export interface HangulizeEndpoint {
-  hangulize: (lang: string, word: string) => Promise<string>
+  hangulize: (lang: string, word: string, traceFn?: (t: HangulizeTrace) => void) => Promise<string>
   getVersion: () => Promise<string>
   getSpecs: () => Promise<Specs>
   useTranslit: (method: string, fn: (word: string) => Promise<string>) => Promise<void>
 }
 
 const endpoint: HangulizeEndpoint = {
-  async hangulize(lang: string, word: string) {
+  async hangulize(lang: string, word: string, traceFn?: (t: HangulizeTrace) => void) {
     await ready
-    return await hangulize(lang, word)
+    return await hangulize(lang, word, traceFn)
   },
 
   async getVersion() {
